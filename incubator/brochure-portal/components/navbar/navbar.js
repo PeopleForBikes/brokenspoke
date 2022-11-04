b4b0af456c578d4081c4c127abdbb12911b6a35a
Rémy Greinhofer <remy.greinhofer@gmail.com>
2022-11-04 18:33:47 -0500

    Update scorecard portal navbar
    Updates the navbar of the scorecard portal to reflect the correct name.
    
    Signed-off-by: Rémy Greinhofer <remy.greinhofer@gmail.com>

--- incubator/brochure-portal/components/navbar/navbar.js
+++ incubator/brochure-portal/components/navbar/navbar.js
@@ -7,7 +7,7 @@ export default function Navbar() {
       <div className=" mx-auto flex flex-wrap p-1 flex-col md:flex-row items-center bg-pfb-deep-navy">
         <Image src={BNALogo} alt="BNA Logo with white text"></Image>
         <a className="flex title-font font-medium items-center text-gray-900 mb-1 md:mb-0">
-          <span className="ml-3 text-4xl text-white">BNA Brochures</span>
+          <span className="ml-3 text-4xl text-white">BNA Scorecards</span>
         </a>
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center"></nav>
       </div>
