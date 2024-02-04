ae99a3c9bc36298768e4cb51b5da2dd750d6749b
Rémy Greinhofer <remy.greinhofer@gmail.com>
2024-02-04 17:52:55 -0600

    Add API calls to monitor pipeline
    Adds API calls to update and monitor the progress of the Brokenspoke
    Analyzer pipeline during its execution.
    
    Drive-by:
    - Consolidates common workspace dependencies.
    
    Signed-off-by: Rémy Greinhofer <remy.greinhofer@gmail.com>

--- lambdas/src/lib.rs
+++ lambdas/src/lib.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct AnalysisParameters {
@@ -9,6 +10,7 @@ pub struct AnalysisParameters {
 }
 
 impl AnalysisParameters {
+    /// Create a new AnalysisParameter object.
     pub fn new(
         country: String,
         city: String,
@@ -19,10 +21,46 @@ impl AnalysisParameters {
             country,
             city,
             region,
-            fips_code: match fips_code {
-                Some(fips_code) => Some(fips_code),
-                None => Some("0".to_string()),
-            },
+            fips_code: fips_code.or(Some("0".to_string())),
         }
     }
+
+    /// Create a new simple AnalysisParameter object with only a city and a country.
+    pub fn simple(country: String, city: String) -> Self {
+        Self::new(country, city, None, None)
+    }
+
+    /// Create a new AnalysisParameter object with a city, a country, and a region.
+    pub fn with_region(country: String, city: String, region: String) -> Self {
+        Self::new(country, city, Some(region), None)
+    }
+
+    /// Create a new AnalysisParameter object with a city, a country, a region and a FIPS code.
+    pub fn with_fips_code(
+        country: String,
+        city: String,
+        region: String,
+        fips_code: String,
+    ) -> Self {
+        Self::new(country, city, Some(region), Some(fips_code))
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub enum BrokenspokeState {
+    Analysis,
+    Export,
+    Pipeline,
+    Setup,
+    SqsMessage,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct BrokenspokePipeline {
+    pub state: Option<BrokenspokeState>,
+    pub state_machine_id: Option<String>,
+    pub sqs_message: Option<String>,
+    pub neon_branch_id: Option<String>,
+    pub fargate_task_id: Option<Uuid>,
+    pub s3_bucket: Option<String>,
 }
